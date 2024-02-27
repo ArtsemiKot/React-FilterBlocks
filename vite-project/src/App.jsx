f81c71dded829111c655e56e8973f5ecea181b28
ArtsemiKot <kot_artemiy@mail.ru>
2024-02-27 23:40:24 +0300

    react-filterblocks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,13 @@
 import Preview from './pages/Preview/Preview'
-
+import Vacansy from './pages/Vacansy/Vacansy'
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <Preview></Preview>
+      <Routes>
+        <Route path="/" element={<Preview />}></Route>
+        <Route path="/:id" element={<Vacansy />}></Route>
+      </Routes>
     </>
   )
 }
--- vite-project/src/Components/List/ListJob.jsx
+++ vite-project/src/Components/List/ListJob.jsx
@@ -1,56 +1,32 @@
-import {IconMapPin } from "@tabler/icons-react";
+import { IconMapPin } from "@tabler/icons-react";
 import style from './style.module.scss'
 import { Pagination } from '@mantine/core';
+import listBlock from "../../Storage/Storage.json";
+import { Link } from "react-router-dom";
+
 function ListJob() {
-    const listBlock = [
-        {
-            id: 1,
-            title: 'Менеджер-дизайнер',
-            salary: 'з/п от 70000 rub',
-            workday: 'Полный рабочий день',
-            location: 'Новый Уренгой'
-        },
-        {
-            id: 2,
-            title: 'Ведущий графический дизайнер НЕ УДАЛЕННО',
-            salary: 'з/п от 80000 rub',
-            workday: 'Полный рабочий день',
-            location: 'Москва'
-        },
-        {
-            id: 3,
-            title: 'Работник декорации, дизайнер (ТЦ Амбар)',
-            salary: 'з/п 29000 rub',
-            workday: 'Сменный график работы',
-            location: 'Самара'
-        },
-        {
-            id: 4,
-            title: 'Менеджер-дизайнер',
-            salary: 'з/п 55000 - 95000 rub',
-            workday: 'Полный рабочий день',
-            location: 'Тюмень'
-        },
-    ]
+
     return (
-        <div className={style.list}>
-            {listBlock.map((el) => {
-                return <div className={style.wrapper}>
-                    <h2>{el.title}</h2>
-                    <div className={style.info}>
-                        <p>{el.salary}</p>
-                        <p className={style.worktime}>{el.workday}</p>
-                    </div>
-                    <div className={style.flexLocation}>
-                        <IconMapPin></IconMapPin>
-                    <p>{el.location}</p>
-                    </div>
+            <div className={style.list}>
+                {listBlock.map((el) => {
+                    return <Link to={`/${el.id}`}>
+                        <div key={el.id} className={style.wrapper}>
+                            <h2>{el.title}</h2>
+                            <div className={style.info}>
+                                <p>{el.salary}</p>
+                                <p className={style.worktime}>{el.workday}</p>
+                            </div>
+                            <div className={style.flexLocation}>
+                                <IconMapPin></IconMapPin>
+                                <p>{el.location}</p>
+                            </div>
+                        </div>
+                    </Link>
+                })}
+                <div>
+                    <Pagination total={7} />
                 </div>
-            })}
-            <div>
-            <Pagination total={4} />;
             </div>
-        </div>
     );
 }
 
